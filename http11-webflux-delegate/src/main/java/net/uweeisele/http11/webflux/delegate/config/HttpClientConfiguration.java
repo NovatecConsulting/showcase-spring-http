80b6c642f549ed9427dca134ebdc46519dabb9ff
Uwe Eisele <code@uweeisele.eu>
2021-05-08 19:27:43 +0200

    webflux client metrics

--- http11-webflux-delegate/src/main/java/net/uweeisele/http11/webflux/delegate/config/HttpClientConfiguration.java
+++ http11-webflux-delegate/src/main/java/net/uweeisele/http11/webflux/delegate/config/HttpClientConfiguration.java
@@ -2,9 +2,9 @@ package net.uweeisele.http11.webflux.delegate.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -22,14 +22,16 @@ public class HttpClientConfiguration {
 
     @Bean
     public HttpClient httpClient() {
-        return HttpClient.create()
-                .metrics(true, uri -> uri);
+        return HttpClient.create().metrics(true, uri -> uri);
     }
 
     @Bean
-    public WebClient webClient(HttpClient httpClient) {
-        return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
+    public WebClientCustomizer httpClientCustomizer(HttpClient httpClient) {
+        return (builder) -> builder.clientConnector(new ReactorClientHttpConnector(httpClient));
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder.build();
     }
 }
